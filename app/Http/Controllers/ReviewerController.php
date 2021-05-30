c647c5ed9979ce1f8168d74fc8093b56e1f0e1cc
Didi Abdillah HMDev <didiabdillah@users.noreply.github.com>
2021-05-30 10:45:30 +0700

    Retrive Dan Tampilkan Data Untuk Halaman Reviewer Di Admin

--- app/Http/Controllers/ReviewerController.php
+++ app/Http/Controllers/ReviewerController.php
@@ -5,10 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
+use App\Models\User;
+
 class ReviewerController extends Controller
 {
     public function index()
     {
-        return view('reviewer.index');
+        $user = User::where('user_role', '=', 'reviewer')
+            ->where('user_id', '!=', Session::get('user_id'))
+            ->orderBy('user_name', 'asc')->get();
+
+        return view('reviewer.index', ['user' => $user]);
     }
 }
