0904cdc2cb3a45d1d6cebd4586154ddec472fa5d
Didi Abdillah HMDev <didiabdillah@users.noreply.github.com>
2021-07-30 18:21:47 +0700

    fix banned banner word

--- resources/views/layout/layout_admin.blade.php
+++ resources/views/layout/layout_admin.blade.php
@@ -40,7 +40,7 @@
         @php $is_suspended = App\Models\User::select('user_ban')->where('user_id', Session::get('user_id'))->first()->user_ban; @endphp
         @if($is_suspended)
         <div class="alert alert-danger main-header layout-navbar-fixed mt-5 pt-3" role="alert">
-            <b><i class="icon fas fa-ban"></i> Akun Anda Ditangguhkan!</b> Beberapa Akses Fitur Tidak Dapat Digunakan... Silahkan Menghubungi Admin Untuk Melakukan Pemulihan Akun
+            <b><i class="icon fas fa-ban"></i> Akses Fitur Tidak Dapat Digunakan...</b> Silahkan Menghubungi Admin
         </div>
         @endif
 
